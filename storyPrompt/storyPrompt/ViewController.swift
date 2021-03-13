1c26b14cce77269ddf932b322ec7e1adbe3eed6c
LuizRamos <luizramospe@hotmail.com>
2021-03-13 19:57:12 -0300

    Dismiss the Keyboard Learning how to dismiss it and what it means to be a first responder.
    Extended the UITextFieldDelegate on ViewController to override the function textFieldShouldReturn.
    Added the ViewController as delegate for the textfields on viewDidLoad method.
    Add the action generateStoryPrompt from the button and create de method updateStoryPrompt

--- storyPrompt/storyPrompt/ViewController.swift
+++ storyPrompt/storyPrompt/ViewController.swift
@@ -7,14 +7,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
+    //MARK: - Outlets
     @IBOutlet weak var nounTextField: UITextField!
     @IBOutlet weak var adjectiveTextField: UITextField!
     @IBOutlet weak var verbTextField: UITextField!
     @IBOutlet weak var numberSlider: UISlider!
     @IBOutlet weak var numberLabel: UILabel!
-    
+
+
+    //MARK: - Actions
     @IBAction func changeNumber(_ sender: UISlider) {
         numberLabel.text = "Number: \(Int(sender.value))"
         storyPrompt.number = Int(sender.value)
@@ -28,21 +31,47 @@ class ViewController: UIViewController {
         }
     }
 
-    //Proprieties
+    @IBAction func generateStoryPrompt(_ sender: Any) {
+        updateStoryPrompt()
+        dump(storyPrompt)
+    }
+
+
+    //MARK: - Proprieties
     let storyPrompt = StoryPromptEntry()
 
+    //MARK: - Overrrided Functions
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         numberSlider.value = 7.5
 
         storyPrompt.noun = "toaster"
         storyPrompt.adjective = "smelly"
         storyPrompt.verb = "burps"
         storyPrompt.number = 10
+
+        //Setting the textField delegates
+        nounTextField.delegate = self
+        verbTextField.delegate = self
+        adjectiveTextField.delegate = self
+        
         dump(storyPrompt)
     }
 
-
+    //MARK: - Functions
+    func updateStoryPrompt(){
+        storyPrompt.noun = nounTextField.text ?? ""
+        storyPrompt.adjective = adjectiveTextField.text ?? ""
+        storyPrompt.verb = verbTextField.text ?? ""
+    }
 }
 
+//MARK: - Extensions
+extension ViewController: UITextFieldDelegate {
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        updateStoryPrompt()
+        return true
+    }
+}
